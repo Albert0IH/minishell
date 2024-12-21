f4a846c7769d13e98c1547f677bdf783fe6744ae
Albert0IH <albertoamuyela@gmail.com>
2024-12-22 00:11:37 +0100

    extracting fixed

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -1,6 +1,60 @@
 #include "../includes/minishell.h"
 
-char **parse_input(char * args)
+char *extract_quote(char **input, char quote)
+{
+    char *start;
+    char *word;
+    size_t len;
+
+    start = *input;
+    len = 0;
+    while (**input && **input != quote)
+    {
+        (*input)++;
+        len++;
+    }
+    word = ft_substr(start, 0, len);
+    if (**input == quote)
+        (*input)++;
+    return (word);
+}
+
+char *extract_word(char **input)
+{
+    char *start;
+    char *word;
+    size_t len;
+
+    start = *input;
+    len = 0;
+    while (**input && **input != ' ' && **input != '\'' && **input != '"')
+    {
+        (*input)++;
+        len++;
+    }
+    word = ft_substr(start, 0, len);
+    return (word);
+}
+
+void extracting(char **input, char **tokens, int i)
+{
+    while (**input == ' ')
+        (*input)++;
+    if (**input =='\'')
+    {
+        (*input)++;
+        tokens[i] = extract_quote(input, '\'');
+    }
+    else if (**input == '"')
+    {
+        (*input)++;
+        tokens[i] = extract_quote(input, '"');
+    }
+    else
+        tokens[i] = extract_word(input);
+}
+
+char **parse_input(char *input)
 {
     char **tokens;
     int i;
@@ -9,60 +63,8 @@ char **parse_input(char * args)
     tokens = malloc(sizeof(char *) * (MAX_ARGS + 1));
     if (!tokens)
         return (NULL);
-    while (*args)
-    {
-        while (*args == ' ')
-            args++;
-        if (*args =='\'')
-        {
-            args++;
-            tokens[i++] = extract_quote(&args, '\'');
-        }
-        else if (*args == '"')
-        {
-            args++;
-            tokens[i++] = extract_quote(&args, '"');
-        }
-        else
-            tokens[i++] = extract_word(&args);
-    }
+    while (*input)
+        extracting(&input, tokens, i++);
     tokens[i] = NULL;
     return(tokens);
 }
-
-char *extract_quote(char **args, char quote)
-{
-    char *start;
-    char *word;
-    size_t len;
-
-    start = *args;
-    len = 0;
-    while (**args && **args != quote)
-    {
-        (*args)++;
-        len++;
-    }
-    word = ft_substr(start, 0, len);
-    if (**args == quote)
-        (*args)++;
-    return (word);
-}
-
-char *extract_word(char **args)
-{
-    char *start;
-    char *word;
-    size_t len;
-
-    start = *args;
-    len = 0;
-    while (**args && **args != ' ' && **args != '\'' && **args != '"')
-    {
-        (*args)++;
-        len++;
-    }
-    word = ft_substr(start, 0, len);
-    return (word);
-}
-
